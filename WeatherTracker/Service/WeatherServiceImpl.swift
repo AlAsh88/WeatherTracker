b9f0e57acc7112bf65f3459ab076fd67bdd3cf1d
AlAsh88 <shk.ayesha@hotmail.com>
2024-12-17 20:05:47 -0500

    Implement weather service with protocol and fetch weather func

---
+++ WeatherTracker/Service/WeatherServiceImpl.swift
@@ -0,0 +1,63 @@
+//
+//  WeatherViewModel.swift
+//  WeatherTracker
+//
+//  Created by Ayesha Shaikh on 12/17/24.
+//
+
+import Foundation
+
+protocol WeatherService {
+    func fetchWeather(forCity city: String, completion: @escaping (Result<WeatherModel, Error>) -> Void)
+}
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingFailed
+}
+
+class WeatherVServiceImpl: WeatherService {
+    private let baseURL = "https://api.weatherapi.com/v1/current.json"
+    private let apiKey = "dcfc7e47d59e41ea949160412241712&q"
+    
+    func fetchWeather(forCity city: String, completion: @escaping (Result<WeatherModel, any Error>) -> Void) {
+        guard var urlComponents = URLComponents(string: baseURL) else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "key", value: apiKey),
+            URLQueryItem(name: "q", value: city)
+        ]
+        
+        guard let url = urlComponents.url else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NetworkError.noData))
+                return
+            }
+            
+            do {
+                let weatherResponse = try JSONDecoder().decode(WeatherModel.self, from: data)
+                completion(.success(weatherResponse))
+            } catch {
+                completion(.failure(NetworkError.decodingFailed))
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
+}
